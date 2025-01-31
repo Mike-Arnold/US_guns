fb08033a31ad6205b14b0e36494da01a3fee5394
Mike-Arnold <53125382+Mike-Arnold@users.noreply.github.com>
2025-01-31 05:40:41 -0600

    add comments to R file

--- US_guns.r
+++ US_guns.r
@@ -5,6 +5,12 @@ options(scipen=999) # don't use scientific notation
 library(dplyr)
 library(ggplot2)
 
+################################################################################
+############ setup
+################################################################################
+
+### import data ###
+
 # https://wonder.cdc.gov/Deaths-by-Underlying-Cause.html
 # choose link for data 2018-2023
 # group by state gender intent
@@ -20,11 +26,26 @@ CDC_import = function(file_name) {
   return(df)
 }
 
+# state abbreviations
 state_ST = read.csv("https://worldpopulationreview.com/static/states/abbr-name.csv",
                     header=F, col.names=c("ST","State")) %>%
   mutate(State = ifelse(ST == "DC", "District of Columbia", State))
 
+# rand survey of gun ownership
 rand = read.csv("RAND_gun_ownership_2016.csv")
+
+## merge the data ##
+
+all_death = CDC_import("CDC/CDC - State Gender Intent External.txt") %>%
+  filter(Injury.Intent %in% c("Suicide","Homicide")) %>%
+  select(State, Gender, Injury.Intent, Deaths, Population) %>%
+  mutate(Rate = Deaths / Population * 10^5) %>%
+  merge(state_ST) %>%
+  merge(rand)
+
+write.csv(all_death, "summary_output.csv", row.names=F)
+
+### graph setup ###
 
 light.color = "#e0e0e0"
 dark.color = "#202020"
@@ -50,32 +71,38 @@ right.label = paste0(
   "Mortality data from CDC, 2018-2023 (wonder.cdc.gov/Deaths-by-Underlying-Cause.html)",
   "\nGun data from RAND, 2016 (rand.org/pubs/tools/TL354.html)")
 
-all_death = CDC_import("CDC/CDC - State Gender Intent External.txt") %>%
-  filter(Injury.Intent %in% c("Suicide","Homicide")) %>%
-  select(State, Gender, Injury.Intent, Deaths, Population) %>%
-  mutate(Rate = Deaths / Population * 10^5) %>%
-  merge(state_ST) %>%
-  merge(rand)
+################################################################################
+############ make graphs
+################################################################################
 
-###### general ######
+### make an individual graph ###
 
 make_graph = function(gender, intent, nudge, y1, y2, y3) {
+  # lower case
   lower.gender = tolower(gender)
   lower.intent = tolower(intent)
   
+  # data frame for the graph
   df = all_death %>%
+    # filter to a specific gender and intent
     filter(Gender == gender, Injury.Intent == intent) %>%
+    # add in label locations (made manually)
     merge(read.csv(paste0("labels/",lower.gender,"_",lower.intent,".csv"))) %>%
+    # sort so large circles are behind small circles
     arrange(desc(Population)) %>%
+    # labels outside a circle are the opposite color
     mutate(Color = ifelse(Color == "light.color", light.color, dark.color))
   
+  # get the trendline and stats
   lm_model = lm(Rate ~ HFR, data = df, weights = Population)
   slope = coef(lm_model)[2]
   correlation = cor(df$HFR, df$Rate)
   
+  # place the annotations
   stats_h = min(df$Rate) + .8 * (max(df$Rate) - min(df$Rate))
   note_h = min(df$Rate) + -0.125 * (max(df$Rate) - min(df$Rate))
   
+  # basic graph
   g1 = ggplot(data = df, aes(x = HFR, y = Rate)) +
     geom_abline(slope = slope, intercept = coef(lm_model)[1], 
                 color = red.color, linetype = "solid", linewidth=2) +
@@ -86,25 +113,33 @@ make_graph = function(gender, intent, nudge, y1, y2, y3) {
     labs(x = "% live with gun", 
          y = paste(gender,lower.intent,"rate"))
   
+  # individual graph with state labels
   g2 = g1 +
+    # make y-axis labels line up with other graphs
     scale_y_continuous(breaks = seq(y1, y2, by = y3),
                        labels = function(x) stringr::str_pad(x, width = 2, side = "left", pad = "\u2007")) +
+    # state labels
     geom_text(aes(label = ST, color = Color), size = 3,
               nudge_x = df$Label.x, 
               nudge_y = df$Label.y) +
+    # notes about the graph
     annotate("text", x = .1, y = note_h, size = 2, color = light.color, hjust=0, vjust=0.5,
              label = left.label) +
     annotate("text", x = .45, y = note_h, size = 2, color = light.color, hjust=0, vjust=0.5,
              label = right.label) +
+    # trendline stats
     annotate("text", x = .09, y = stats_h, size=5,
              label = paste0("Slope: ", format(round(slope,2), nsmall=2, trim=T), "\n",
                             "Correlation: ", format(round(correlation,2), nsmall=2, trim=T)),
              hjust = 0, vjust = 0.5, color=red.color) +
-    
+    # allow notes to go below plot area
     coord_cartesian(ylim = c(min(df$Rate), max(df$Rate)), clip = "off")
   
+  # graph for comparison
   g3 = g1 + 
+    # same y-axis for all
     coord_cartesian(ylim = c(0,45)) +
+    # trendline stats
     annotate("text", x = .1, y = 40, size=5,
              label = paste0("Slope: ", format(round(slope,2), nsmall=2, trim=T), "\n",
                             "Correlation: ", format(round(correlation,2), nsmall=2, trim=T)),
@@ -114,13 +149,17 @@ make_graph = function(gender, intent, nudge, y1, y2, y3) {
   return(g3)
 }
 
+# make individual graphs
 fs = make_graph("Female","Suicide","fs_nudge.csv", 5, 11, 3)
 ms = make_graph("Male","Suicide","ms_nudge.csv", 20, 40, 10)
 fh = make_graph("Female","Homicide","fh_nudge.csv", 0, 6, 2)
 mh = make_graph("Male","Homicide","mh_nudge.csv", 0, 30, 10)
 
+## make comparison graph ##
+# arrange 2x2
 compare = gridExtra::grid.arrange(fs, ms, fh, mh, ncol=2, nrow=2)
 
+# note below comparison
 annotation = ggplot() + 
   annotate("text", x = 0, y = 0.5, label = left.label, size = 3, color = light.color, hjust=0) +
   annotate("text", x = 0.55, y = 0.5, label = right.label, size = 3, color = light.color, hjust=0) +
@@ -131,6 +170,7 @@ annotation = ggplot() +
   ) +
   coord_cartesian(xlim = c(0, 1), ylim = c(0, 1))
 
+# attach note and save
 compare2 = gridExtra::grid.arrange(compare, annotation, ncol = 1, heights = c(16, 1))
 ggsave("images/comparison.png", compare2, width=12, height=9)
 
